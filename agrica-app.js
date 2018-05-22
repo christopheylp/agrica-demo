dd1e90bc99319c2760f1feba612517b705214e5e
YALAP Christophe <yalap.christophe@groupagrica.com>
2018-05-22 16:14:09 +0200

    test

---
+++ agrica-app.js
@@ -0,0 +1,46 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+import './agrica-demo.js';
+import './agrica-list-app.js';
+
+/**
+ * `agrica-app`
+ * 
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class AgricaApp extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <agrica-demo on-add-app="handleSaveEvent" ></agrica-demo>
+      <agrica-list-app apps="[[apps]]"></agrica-list-app>
+    `;
+  }
+
+  static get properties() {
+    return {
+      apps: {
+            type: Array,
+            value: function () {
+                return [
+                    {name: "Name", description: "Desc"},
+                ]
+            },
+        },
+    };
+  }
+
+  handleSaveEvent(e) {
+    const data =  e.detail.data;
+    this.push('apps', data);
+  }
+}
+
+window.customElements.define('agrica-app', AgricaApp);
